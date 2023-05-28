07657ae36bd31364d8c47cf0244919cf89a4432f
leejisoo20190998 <85981376+leejisoo20190998@users.noreply.github.com>
2023-05-28 12:55:34 +0900

    Update IMDBStudent20190998.java

--- HW2/IMDBStudent20190998.java
+++ HW2/IMDBStudent20190998.java
@@ -1,5 +1,5 @@
-import java.io.IOException;
 import java.util.*;
+import java.io.*;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -11,72 +11,121 @@ import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
 
-public class IMDBStudent20190998
-{
-	public static class IMDBMapper extends Mapper<LongWritable, Text, Text, LongWritable> 
-	{
-		private final LongWritable one = new LongWritable(1);
-		private Text word = new Text();
-		
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			String[] split = value.toString().split("::");
-			if (split.length < 3) {
-				return;
-			}
+class Emp {
+	public int id;
+	public int salary;
+	public String dept_id;
+	public String emp_info;
+	
+	public Emp(int _id, int _salary, String _dept_id, String _emp_info) {
+		this.id = _id;
+		this.salary = _salary;
+		this.dept_id = _dept_id;
+		this.emp_info = _emp_info;
+	}
+
+	public String getString() {
+		return id + "|" + dept_id + "|" + salary + "|" + emp_info;
+	}
+}	
+
+public class IMDBStudent20190998 {
+	public static class EmpComparator implements Comparator<Emp> {
+		public int compare(Emp x, Emp y) {
+			if ( x.salary > y.salary ) return 1;
+			if ( x.salary < y.salary ) return -1;
 			
-			String genre = split[split.length - 1];
-			
-			StringTokenizer tokenizer = new StringTokenizer(genre, "|");
-			while (tokenizer.hasMoreTokens()) {
-				word.set(tokenizer.nextToken());
-				context.write(word, one);
-			}
+			return 0;
 		}
-		
 	}
 	
-	public static class IMDBReducer extends Reducer<Text, LongWritable, Text, LongWritable>
-	{
-		private LongWritable sumWritable = new LongWritable();
-		
-		public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (LongWritable val : values) {
-				sum += val.get();
-			}
+	public static void insertEmp(PriorityQueue q, int id, int salary, String dept_id, String emp_info, int topK) {
+			Emp emp_head = (Emp) q.peek();
 			
-			sumWritable.set(sum);
-			context.write(key, sumWritable);
+			if ( q.size() < topK || emp_head.salary < salary ) {
+				Emp emp = new Emp(id, salary, dept_id, emp_info);
+				q.add( emp );
+				
+				if( q.size() > topK ) q.remove();
+			}
+	}
+	
+	public static class TopKMapper extends Mapper<Object, Text, Text, NullWritable> {
+		private Comparator<Emp> comp = new EmpComparator();
+		private PriorityQueue<Emp> queue;
+		private int topK;
+		
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString(), "|");
+			int emp_id = Integer.parseInt( itr.nextToken().trim());
+			String dept_id = itr.nextToken().trim();
+			int salary = Integer.parseInt( itr.nextToken().trim() );
+			String emp_info = itr.nextToken().trim();
+		
+			insertEmp(queue, emp_id, salary, dept_id, emp_info, topK);
+		}
+	
+		protected void setup(Context context) throws IOException, InterruptedException {
+			Configuration conf = context.getConfiguration();
+			topK = conf.getInt("topK", -1);
+			queue = new PriorityQueue<Emp>( topK , comp);
+		}
+		
+		protected void cleanup(Context context) throws IOException, InterruptedException {
+			while( queue.size() != 0 ) {
+				Emp emp = (Emp) queue.remove();
+				context.write( new Text( emp.getString() ), NullWritable.get() );
+			}
+		}
+	}
+	
+	public static class TopKReducer extends Reducer<Text,NullWritable,Text,NullWritable> {
+		private PriorityQueue<Emp> queue;
+		private Comparator<Emp> comp = new EmpComparator();
+		private int topK;
+		
+		public void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(key.toString(), "|");
+			int emp_id = Integer.parseInt( itr.nextToken().trim());
+			String dept_id = itr.nextToken().trim();
+			int salary = Integer.parseInt( itr.nextToken().trim() );
+			String emp_info = itr.nextToken().trim();
+			insertEmp(queue, emp_id, salary, dept_id, emp_info, topK);
+		}
+		
+		protected void setup(Context context) throws IOException, InterruptedException {
+			Configuration conf = context.getConfiguration();
+			topK = conf.getInt("topK", -1);
+			queue = new PriorityQueue<Emp>( topK , comp);
+		}
+		
+		protected void cleanup(Context context) throws IOException, InterruptedException {
+			while( queue.size() != 0 ) {
+				Emp emp = (Emp) queue.remove();
+				context.write( new Text( emp.getString() ), NullWritable.get() );
+			}
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length != 2)
-		{
-			System.err.println("Usage: IMDB <in> <out>");
-			System.exit(2);
+		int topK = 3;
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: TopK <in> <out>"); System.exit(2);
 		}
-	
-		Job job = new Job(conf, "IMDB");
-
-		job.setJarByClass(IMDBStudent20190998.class);
-		job.setMapperClass(IMDBMapper.class);
-		job.setReducerClass(IMDBReducer.class);
-		job.setCombinerClass(IMDBReducer.class);
-	
+		conf.setInt("topK", topK);
+		Job job = new Job(conf, "TopK");
+		job.setJarByClass(TopK.class);
+		job.setMapperClass(TopKMapper.class);
+		job.setReducerClass(TopKReducer.class);
+		job.setNumReduceTasks(1);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(LongWritable.class);
-	
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-	
+		job.setOutputValueClass(NullWritable.class);
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));	
-    	
-		FileSystem.get(job.getConfiguration()).delete(new Path(otherArgs[1]), true);
-
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
+	
 }
